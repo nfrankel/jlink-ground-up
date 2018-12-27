7ce1520a2568b1212b29473ba7e4cc88a12d2c99
Nicolas Frankel <nicolas@frankel.ch>
2018-12-27 23:35:06 +0100

    Add Commons Lang 3 dependency

--- src/main/java/ch/frankel/blog/jlink/Main.java
+++ src/main/java/ch/frankel/blog/jlink/Main.java
@@ -1,14 +1,19 @@
 package ch.frankel.blog.jlink;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        String message = "Hello world";
+        String hello = "Hello ";
+        String world = "world! ";
+        String from = "from Commons Lang";
+        String message = StringUtils.join(hello, world, from);
         LOGGER.info(message);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module ch.frankel.jlink {
     requires org.slf4j;
+    requires org.apache.commons.lang3;
     exports ch.frankel.blog.jlink;
 }
