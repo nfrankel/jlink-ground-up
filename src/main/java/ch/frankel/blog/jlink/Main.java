b9d8a6eab5041a17601d799835e1b388a45dc611
Nicolas Frankel <nicolas@frankel.ch>
2018-12-27 22:28:27 +0100

    Replace Apache Commons Lang by Guava

--- src/main/java/ch/frankel/blog/jlink/Main.java
+++ src/main/java/ch/frankel/blog/jlink/Main.java
@@ -1,6 +1,6 @@
 package ch.frankel.blog.jlink;
 
-import org.apache.commons.lang3.StringUtils;
+import com.google.common.base.Joiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,10 +10,10 @@ public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        String hello = "Hello ";
-        String world = "world! ";
-        String from = "from Commons Lang";
-        String message = StringUtils.join(hello, world, from);
+        String hello = "Hello";
+        String world = "world!";
+        String from = "from Guava";
+        String message = Joiner.on(" ").join(hello, world, from);
         LOGGER.info(message);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module ch.frankel.jlink {
     requires org.slf4j;
-    requires org.apache.commons.lang3;
+    requires com.google.common;
     exports ch.frankel.blog.jlink;
 }
